974750d5b4bb316154235de4d99c457e461b098f
MuhammadTayyebHamid <159242117+MuhammadTayyebHamid@users.noreply.github.com>
2024-03-22 17:13:27 +0500

    Create Society.cs

---
+++ Models/Society.cs
@@ -0,0 +1,12 @@
+﻿namespace SE_Project.Models
+{
+    public class Society
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MentorName { get; set; }
+        public string[] TeamMembers { get; set; }
+        public string Head { get; set; }
+        public string Plans { get; set; }
+    }
+}
