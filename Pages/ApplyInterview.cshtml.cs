e9789f7d5b8e72b3fef8097e3bf30f667f903cf4
huzaifatahirrathore <huzaifatahirrathore86@gmail.com>
2024-03-31 01:12:59 +0500

    till ManageInterview

---
+++ Pages/ApplyInterview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE_Project.Pages
+{
+    public class ApplyInterviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
