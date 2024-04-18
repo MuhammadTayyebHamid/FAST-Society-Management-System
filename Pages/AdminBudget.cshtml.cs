9027bd030a7a1b83df144611bb12b1efcfa27a75
huzaifatahirrathore <huzaifatahirrathore86@gmail.com>
2024-04-18 22:59:19 +0500

    till Budget and HomePage

---
+++ Pages/AdminBudget.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE_Project.Pages
+{
+    public class AdminBudgetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
